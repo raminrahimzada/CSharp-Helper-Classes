32c8afc244d7c2222edd2ed53e946490b3afa753
raminrahimzada <raminrahimzada@gmail.com>
2017-05-18 16:22:04 +0400

    Add files via upload

---
+++ SessionManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using System.Web;
+
+namespace raminrahimzada
+{
+    public class SessionManager
+    {
+        private static T Get<T>(string key)
+        {
+            object sessionObject = HttpContext.Current.Session[key];
+            if (sessionObject == null)
+            {
+                return default(T);
+            }
+            return (T)HttpContext.Current.Session[key];
+        }
+        private static void Save<T>(string key, T entity)
+        {
+            HttpContext.Current.Session[key] = entity;
+        }
+
+        private static void Remove(string key)
+        {
+            HttpContext.Current.Session.Remove(key);
+        }
+        public static string LoggedUserName
+        {
+            set
+            {
+                if (value != null) Save("LoggedUserName", value);
+                else
+                    Remove("LoggedUserName");
+            }
+            get { return Get<string>("LastErrorString"); }
+        }         
+        public static void RemoveAllSession()
+        {
+            HttpContext.Current.Session.Clear();
+        }
+    }    
+}
